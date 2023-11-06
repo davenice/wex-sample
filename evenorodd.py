3e716eaddb74079832a15b99e2e8f5098476d0b3
Emily N <emily@niceweb.org.uk>
2023-11-06 11:46:34 +0000

    simplified_if_statement_updated

--- evenorodd.py
+++ evenorodd.py
@@ -1,5 +1,5 @@
 
-# returns the word even if the provided number is even, or odd if it's odd
+# returns the word 'even' if the provided number is even, or 'odd' if it's odd
 def even_or_odd(number):
     result = number%2
     if result == 0:
