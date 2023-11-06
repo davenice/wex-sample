99eab124f8027e438b321d818c320414d7bfe868
Emily N <emily@niceweb.org.uk>
2023-11-06 11:47:57 +0000

    neaten it up

--- evenorodd.py
+++ evenorodd.py
@@ -1,7 +1,7 @@
 
 # returns the word 'even' if the provided number is even, or 'odd' if it's odd
 def even_or_odd(number):
-    result = number%2
+    result = number % 2
     if result == 0:
         return "even"
     else:
